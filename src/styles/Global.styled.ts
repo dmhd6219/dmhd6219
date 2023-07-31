ced30f1a879b8581c57eb83060d152b48871a4e7
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-31 15:21:26 +0300

    add heading

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -1,4 +1,4 @@
-import styled, {DefaultTheme} from "styled-components";
+import styled from "styled-components";
 
 export const MainBody = styled.div`
   background-color: ${({theme}) => theme.colors.primary};
