264c56eb1eb813a7d7429430d5131573021f889d
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-08-01 17:36:18 +0300

    remove warnings

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -134,23 +134,6 @@ export const IconContainer = styled.a<IconContainerProps>`
   }}
 `
 
-export const Button = styled.a`
-  display: inline-block;
-  width: max-content;
-  padding: 1rem 2rem;
-  color: ${({theme}) => theme.colors.white};
-  background-color: ${({theme}) => theme.colors.primary_light};
-  border: 1px solid ${({theme}) => theme.colors.gray};
-  border-radius: 5px;
-  cursor: pointer;
-  transition: all 0.3s ease;
-
-  &:hover {
-    color: ${({theme}) => theme.colors.primary_light};
-    background-color: ${({theme}) => theme.colors.white};
-  }
-`
-
 
 export interface FadeImage {
     top?: string,
