d3a37288db59fec44bb43c7be5edad2edcdab278
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-05-20 16:23:05 +0800

    feat: add 'me' section

--- src/components/page/Layout/Layout.tsx
+++ src/components/page/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/page/Header';
 import { Footer } from '@/components/page/Footer';
 import { About } from '@/components/page/Layout/components/About';
+import { Me } from '@/components/page/Layout/components/Me';
 
 const Layout = () => {
     return (
         <div className="container mx-auto flex flex-col text-white h-full dark">
             <Header />
             <div className="flex-1">
+                <Me />
                 <About />
             </div>
             <Footer />
---
+++ src/components/page/Layout/components/Me/Me.tsx
@@ -0,0 +1,37 @@
+const Me = () => {
+    return (
+        <div
+            className="mt-32 mb-16 mx-auto max-w-screen-lg text-center font-bold text-4xl md:text-5xl lg:text-6xl"
+            id="home"
+        >
+            <h1>
+                Привет! Меня зовут{' '}
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-700 to-rose-600">
+                    Святослав
+                </span>
+                , <br />Я{' '}
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-rose-700 to-pink-600">
+                    Frontend
+                </span>{' '}
+                разработчик, создающий современные веб-приложения.
+            </h1>
+            <span className="mt-5 text-lg text-muted-foreground max-w-screen-md block mx-auto">
+                На данный момент я учусь в{' '}
+                <a
+                    href="https://innopolis.university/"
+                    target="_blank"
+                    className="text-innopolis"
+                >
+                    Университете Иннополис
+                </a>{' '}
+                на 4м курсе
+                {/*и работаю Frontend разработчиком в {' '}*/}
+                {/*<a href="https://yandex.ru/" target="_blank" className='text-yandex'>*/}
+                {/*    компания-нейм*/}
+                {/*</a>*/}.
+            </span>
+        </div>
+    );
+};
+
+export default Me;
